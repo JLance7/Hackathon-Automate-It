839ce56f8d330908379039e3e06ccb17c38fe5a0
Josh L <jlwnf@umsystem.edu>
2021-10-16 15:41:38 -0500

    even more progress

--- flaskApp/py/scrape.py
+++ flaskApp/py/scrape.py
@@ -2,42 +2,59 @@ import requests
 from bs4 import BeautifulSoup
 
 num_of_articles = 2
-url = "https://www.nbcnews.com/"
+
 
 titles = []
 links = []
 article_text = []
 
-r1 = requests.get(url)
-coverpage = r1.content
-soup1 = BeautifulSoup(coverpage, 'html5lib')
-title_content = soup1.find_all('span', class_='tease-card__headline')
-article_content = soup1.find_all('div', class_='article-body__content')
-link_content = soup1.find_all('h2', class_='tease-card__headline tease-card__title relative')
 
-def getNews():
-    for i in range(0, num_of_articles):
-        #get titles
-        title = title_content[i].get_text()
-        titles.append(title)
-        #get links
-        link = link_content[i].find('a')['href']
-        links.append(link)
-        #get articles
-        article = requests.get(link)
-        article_con = article.content
-        soup_article = BeautifulSoup(article_con, 'html5lib')
-        body = soup_article.find_all('div', class_='article-body__content')
-        x = body[0].find_all('p')
 
-        # Unifying the paragraphs
-        list_paragraphs = []
-        for p in range(0, len(x)):
-            paragraph = x[p].get_text()
-            list_paragraphs.append(paragraph)
-            final_article = " ".join(list_paragraphs)
-            
-        article_text.append(final_article)
+def getNews(url):
+    r1 = requests.get(url)
+    coverpage = r1.content
+    soup1 = BeautifulSoup(coverpage, 'html5lib')
+    if url == "https://www.nbcnews.com/":
+        title_content = soup1.find_all('span', class_='tease-card__headline')
+        article_content = soup1.find_all('div', class_='article-body__content')
+        link_content = soup1.find_all('h2', class_='tease-card__headline tease-card__title relative')
+    elif url == "https://www.cnn.com/":
+        title_content = soup1.find_all('', class_='')
+        article_content = soup1.find_all('', class_='')
+        link_content = soup1.find_all('', class_='')
+    else:
+        title_content = soup1.find_all('', class_='')
+        article_content = soup1.find_all('', class_='')
+        link_content = soup1.find_all('', class_='')
+    end = False
+    while (end == False):
+        try:
+            for i in range(0, num_of_articles):
+                #get titles
+                title = title_content[i].get_text()
+                titles.append(title)
+                #get links
+                link = link_content[i].find('a')['href']
+                links.append(link)
+                #get articles
+                article = requests.get(link)
+                article_con = article.content
+                soup_article = BeautifulSoup(article_con, 'html5lib')
+                body = soup_article.find_all('div', class_='article-body__content')
+                x = body[0].find_all('p')
+
+                # Unifying the paragraphs
+                list_paragraphs = []
+                for p in range(0, 5):
+                    paragraph = x[p].get_text()
+                    list_paragraphs.append(paragraph)
+                    final_article = " ".join(list_paragraphs)
+
+                
+                article_text.append(final_article)
+                end = True
+        except:
+            print('Error occurred while scraping')
 
 def getLinks():
     return links
@@ -45,9 +62,10 @@ def getLinks():
 def getTitles():
     return titles
 
-def getText():
-    return article_content
+def getArticleText():
+    return article_text
 
-
+def setNumOfArticles(num):
+    num_of_articles = num
 
 
--- flaskApp/py/site.py
+++ flaskApp/py/site.py
@@ -2,7 +2,9 @@
 from flask import Flask, redirect, url_for, render_template
 from scrape import *
 #from newsGrabber import *
-
+links = None
+titles = None
+texts = None
 
 app = Flask(__name__)
 app = Flask(__name__, static_folder='../static', template_folder='../html')
@@ -11,15 +13,17 @@ app = Flask(__name__, static_folder='../static', template_folder='../html')
 @app.route("/")
 def home():
     try:
-        return render_template("index.html", page="Reliable News", list=["blah", "blah", "blah", "blah", "blah", "blah", "blah", "blah"])
+        return render_template("index.html", page="Reliable News", list1=titles)
     except:
         print('\nerror in home\n')
 
 #any other subdirectory url given
+#if no subdirectory url is given show reliable news (index.html)
+@app.route("/")
 @app.route("/<name>/")
-def other(name):
+def other():
     try:
-        return redirect(url_for("home"))
+        return redirect(url_for("first_page"))
     except:
         print('\nerror in other func\n')
 
@@ -27,7 +31,7 @@ def other(name):
 @app.route("/reliable-news/")
 def first_page():
     try:
-        return render_template("index.html", page="Reliable News", list1=links, list2=titles, list2=texts)
+        return render_template("index.html", page="Reliable News", list1=links, list2=titles, list3=texts, num=len(links))
     except:
         print('\nerror in first_page func\n')
 
@@ -35,7 +39,7 @@ def first_page():
 @app.route("/unreliable-news/")
 def second_page():
     try:
-        return render_template("second.html", page="Unreliable News", list=["woo", "woo", "woo", "woo", "woo", "woo", "woo", "woo", "woo", "woo"])
+        return render_template("second.html", page="Unreliable News", list1=links, list2=titles, list3=texts, num=len(links))
     except:
         print('\nerror in second_page func\n')
 
@@ -48,12 +52,17 @@ def third_page():
         print('\nerror in third_page func\n')
 
 
-#scrape info info from websites into 
-getNews()
-links = getLinks()
-titles = getTitles()
-texts = getText()
-
+urls = ["https://www.nbcnews.com/", "https://www.cnn.com/", "https://www.foxnews.com/"]
 
 if __name__ == "__main__":
+    #scrape info info from websites into
+    setNumOfArticles(10)
+    for i in range(len(urls)): 
+        getNews(url[i])
+        links += getLinks()
+        titles += getTitles()
+        texts += getArticleText()
+    # print('Titles are: ' + str(titles))
+    # print('Links are: ' + str(links))
+    # print('Bodies are: ' + str(texts))
     app.run(debug=True)
